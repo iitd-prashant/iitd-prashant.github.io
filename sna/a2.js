18f137e69b3144f4f7f7bef5a2acecb65f849013
Prashant Kumar <iitd.prashant@gmail.com>
2020-09-10 23:12:09 +0530

    initial commit

---
+++ sna/a2.js
@@ -0,0 +1,6 @@
+document.writeln('<marquee bgcolor="#F665FF">')
+document.writeln('<a> First Quiz on September 3, 2014 at 12-1 pm. Venue: CR-107</a>');
+document.writeln(' ');
+document.writeln('');
+document.writeln('</marquee>');
+
